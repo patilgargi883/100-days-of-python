0fd5455337735f0215b26cf32fb2a39b20d3ab02
Gargi Patil <148727733+patilgargi883@users.noreply.github.com>
2025-06-11 19:59:53 +0530

    Day 1 - Added 5 Python programs
    Included programs:
    - Reverse Number
    - Reverse String
    - Fibonacci Series
    - Perfect Number Checker
    - GCD of Two Numbers

---
+++ perfect_number_checker.py
@@ -0,0 +1,11 @@
+num = int(input("Enter a number: "))
+sum = 0
+
+for i in range(1, num):
+    if num % i == 0:
+        sum += i
+
+if sum == num:
+    print("It is a perfect number")
+else:
+    print("It is not a perfect number")
